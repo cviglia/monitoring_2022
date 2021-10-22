864ae8181379bcbba19b998b6b783ce92d993a11
cviglia <92572568+cviglia@users.noreply.github.com>
2021-10-22 10:18:57 +0200

    Update R_first_code.r

--- R_first_code.r
+++ R_first_code.r
@@ -7,8 +7,6 @@ water <-c (100, 200, 300, 400, 500)
 # marta data on fishes genomes
 fishes <-c (10, 50, 60, 100, 200)
 
-
-
 # plot the diversity of fishes (y) versus the amount of water (x)
 # a function is used with arguments inside ()
 # plot(water, fishes)
@@ -19,3 +17,10 @@ fishes <-c (10, 50, 60, 100, 200)
 streams <- data.frame(water, fishes)
 
 View(streams)
+
+# we are going to export
+# setwd("C:/lab/")
+setwd("C:/lab/")
+
+# let's export our table
+write.table(streams, file="my_first_table.txt")
