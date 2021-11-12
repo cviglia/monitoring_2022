a6371fd5fa3e1425abda05d240642b2be66958e2
cviglia <92572568+cviglia@users.noreply.github.com>
2021-11-12 10:08:58 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -106,3 +106,27 @@ plotRGB(l2011, r=3, g=2, b=1, stretch="Lin") # natural colours
 plotRGB(l2011, r=4, g=3, b=2, stretch="Lin") # false colours
 plotRGB(l2011, r=3, g=4, b=2, stretch="Lin") # false colours
 plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")
+
+# .................................................................... day 4
+
+library(raster)
+
+setwd("C:/lab/")
+
+l2011 <- brick("p224r63_2011.grd")
+
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Hist")
+
+# importing past data
+l1988 <- brick("p224r63_1988.grd")
+l1988
+
+par(mfrow=c(2,1))
+plotRGB(l1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+
+# put the NIR in the blue channel
+par(mfrow=c(2,1))
+plotRGB(l1988, r=2, g=3, b=4, stretch="Lin")
+plotRGB(l2011, r=2, g=3, b=4, stretch="Lin")
