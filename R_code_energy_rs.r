8f8e20979c7fea6fd428daa10abb4605b51894a3
cviglia <92572568+cviglia@users.noreply.github.com>
2021-11-19 11:01:09 +0100

    Update R_code_energy_rs.r

--- R_code_energy_rs.r
+++ R_code_energy_rs.r
@@ -15,3 +15,9 @@ l1992
 # Bands: defor1.1, defor1.2, defor1.3 
 # plot RGB
 plotRGB(l1992, r=1, g=2, b=3, stretch="Lin")
+
+# defor1.1 = NIR
+# defor1.2 = red
+# defor1.3 = green
+
+plotRGB(l1992, r=1, g=2, b=3, stretch="Lin")
