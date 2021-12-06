ff48d9766a4c9570ee6fe60869d1197b334fd4b8
cviglia <92572568+cviglia@users.noreply.github.com>
2021-12-06 10:55:00 +0100

    Update R_code_quantitative_estimates_land_cover.r

--- R_code_quantitative_estimates_land_cover.r
+++ R_code_quantitative_estimates_land_cover.r
@@ -3,6 +3,7 @@
 library(raster)
 library(RStoolbox) # classification
 library(ggplot2)
+library(gridExtra) # multiframe ggplot
 
 # set working directory
 setwd("C:/lab/") 
@@ -27,7 +28,7 @@ plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
 l2006 <- list_rast[[2]]
 plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
 
-# Unsupervised claffication
+# Unsupervised claffication of 1992
 l1992c <- unsuperClass(l1992, nClasses=2)  # unsuperClass(x, nClasses)
 
 plot(l1992c$map)
@@ -44,8 +45,8 @@ freq(l1992c$map)
 
 # 36016 + 305276 =  341292
 total <- 341292
-propagri <- 36016 / 341292         # 0.11
-propforest <- 305276 / 341292      # 0.89
+propagri <- 36016 / 341292         # ~ 0.11
+propforest <- 305276 / 341292      # ~ 0.89
 
 # build a dataframe
 cover <- c("forest", "agriculture")
@@ -57,8 +58,37 @@ ggplot(proportion1992, aes(x=cover, y=prop1992, color=cover)) + geom_bar(stat="i
 
 
 
+# Unsupervised claffication of 2006
+l2006c <- unsuperClass(l2006, nClasses=2)  # unsuperClass(x, nClasses)
 
+plot(l2006c$map)
+# value 1 = agricultural areas and water
+# value 2 = forests
 
+freq(l2006c$map)
+# value  count
+# [1,]     1 164501
+# [2,]     2 178225
+
+# agricultural areas and water (class1)
+# forests (class2)
+total <- 342726
+propagri2006 <- 164501/342726    # 0.4799782    ~ 0.48
+propforest2006 <- 178225/342726    # 0.5200218  ~ 0.52
+
+# build a dataframe
+cover <- c("forest", "agriculture")
+prop2006 <- c(propforest2006,  propagri2006) 
+
+proportion2006 <- data.frame(cover, prop2006)
+proportion <- data.frame(cover, prop1992, prop2006)
+
+ggplot(proportion, aes(x=cover, y=prop2006, color=cover)) + geom_bar(stat="identity", fill="white")
+
+p1 <- ggplot(proportion1992, aes(x=cover, y=prop1992, color=cover)) + geom_bar(stat="identity", fill="white")
+p2 <- ggplot(proportion, aes(x=cover, y=prop2006, color=cover)) + geom_bar(stat="identity", fill="white")
+
+grid.arrange(p1, p2, nrows=1)
 
 
 
