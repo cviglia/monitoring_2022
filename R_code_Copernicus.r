d87ce3abfc0bbcadd4b514981f2699140b99c103
cviglia <92572568+cviglia@users.noreply.github.com>
2021-12-15 17:55:14 +0100

    Update R_code_Copernicus.r

--- R_code_Copernicus.r
+++ R_code_Copernicus.r
@@ -1,2 +1,13 @@
+# code for Copernicus
 library(ncdf4)
+library(raster)
 
+# set the working directory
+setwd("C:/lab//copernicus/") 
+
+# raster
+leaf01052020 <- raster("c_gls_LAI-RT0_202006300000_GLOBE_PROBAV_V2.0.1.nc")
+leaf01052020
+
+# plot the data
+plot(leaf01052020)
