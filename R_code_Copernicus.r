194d86df7729f5afb45bb42970b46f5ad1d6a606
cviglia <92572568+cviglia@users.noreply.github.com>
2021-12-16 12:17:11 +0100

    Update R_code_Copernicus.r

--- R_code_Copernicus.r
+++ R_code_Copernicus.r
@@ -1,6 +1,9 @@
-# code for Copernicus
+# R code for uploading and visualizing Copernicus data in Rlibrary(ncdf4)
 library(ncdf4)
 library(raster)
+library(ggplot2)
+library(viridis)
+library(RStoolbox)
 
 # set the working directory
 setwd("C:/lab//copernicus/") 
@@ -11,3 +14,20 @@ leaf01052020
 
 # plot the data
 plot(leaf01052020)
+
+cl <- colorRampPalette(c('light green','green','dark green'))(100) #
+plot(leaf01052020, col=cl)
+
+# viridis
+install.packages("viridis")
+library(viridis)
+
+ggplot() + geom_raster(leaf01052020, mapping = aes(x = x, y = y, fill = Leaf.Area.Index.1km))
+# viridis
+ggplot() + geom_raster(leaf01052020, mapping = aes(x = x, y = y, fill = Leaf.Area.Index.1km)) 
++ scale_fill_viridis() +  ggtitle("viridis palette")
+# cividis
+ggplot() + geom_raster(leaf01052020, mapping = aes(x = x, y = y, fill = Leaf.Area.Index.1km)) 
++ scale_fill_viridis(option = "cividis") +  ggtitle("cividis palette")
+
+
