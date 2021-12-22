84df5534be2efdac32a2c4af272c12fca036e2ce
cviglia <92572568+cviglia@users.noreply.github.com>
2021-12-22 12:36:27 +0100

    Update R_code_sdm.r

--- R_code_sdm.r
+++ R_code_sdm.r
@@ -1,1 +1,19 @@
+# R code for Species Distribution Modelling
+
+install.packages("sdm")
+
+library(sdm)
+library(raster) # predictors
+library(rgdal) # species
+
+file <- system.file("external/species.shp", package="sdm")
+file # pathway to data
+species <- shapefile(file) # as the raster function
+species # looking at the set
+
+# plot the species
+plot(species, pch = 19, col = "red")
+
+
+
 
