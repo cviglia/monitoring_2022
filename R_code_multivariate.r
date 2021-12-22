d19789363db0eb6fdfcfc8467ecfb9c435aa266a
cviglia <92572568+cviglia@users.noreply.github.com>
2021-12-22 11:49:32 +0100

    Update R_code_multivariate.r

--- R_code_multivariate.r
+++ R_code_multivariate.r
@@ -3,7 +3,28 @@
 install.packages("vegan")
 library(vegan)
 
-setwd("C:/lab/en/")
+# set the work dir
+setwd("C:/lab/")
 
+# species
+load("biomes_multivar.RData")
+ls()
+
+# plot per species matrix
+head(biomes)
+
+multivar <- decorana(biomes)
+multivar
+
+# plot the data
+plot(multivar)
+
+# biomes names in the graph:
+attach(biomes_types)
+ordiellipse(multivar, type, col = c("black", "red", "green", "blue"), kind = "ehull", lwd =3)
+
+ordispider(multivar, type, col=c("black","red","green","blue"), label=T)
+
+install.packages("sdm")
 
 
