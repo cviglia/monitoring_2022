d2c5772b2cff14fec4d0aeebe40050aeedc57a56
cviglia <92572568+cviglia@users.noreply.github.com>
2022-01-12 09:52:59 +0100

    Update R_code_sdm.r

--- R_code_sdm.r
+++ R_code_sdm.r
@@ -51,6 +51,32 @@ plot(preds$vegetation, col=cl)
 points(presences, pch=19)
 
 # ........................................................................................... day 2
+# importing the source script
+
+library(sdm)
+library(raster)
+library(rgdal)
+
+# set the working directory
+setwd("C:/lab/")
+
+source("R_code_source_sdm.r") # graphs will appear
+
+# in the theoretical slide of SDMs we should use individuals of a species and predictors 
+preds
+# predictors are: elevation, precipitation, temperature, vegetation
+ 
+# set the data for the sdm
+sdmData(train=species, predictors=preds)
+
+# Let's explain to the model what are the training and predictors data
+datasdm <- sdmData(train=species, predictors=preds)
+
+# model
+m1 <- sdm(Occurrence~temperature+elevation+precipitation+vegetation, data=datasdm, methods="glm")
+m1
+
+# ........................................................................................... day 3
 
 library(sdm)
 library(raster)
@@ -58,23 +84,31 @@ library(rgdal)
 
 setwd("C:/lab/")
 
-source("R_code_source_sdm.r") # graphs will appear
-
-# in the theoretical slide of sdms we should use individuals of a species and predictors
-preds
-# predictors are: elevation, precipitation, temperature, vegetation
-
-sdmData(train=species, predictors=preds)
-
 # set the data for the sdm
 datasdm <- sdmData(train=species, predictors=preds)
+datasdm
 
 # model
 m1 <- sdm(Occurrence~temperature+elevation+precipitation+vegetation, data=datasdm, methods="glm")
 m1
 
+# make the raster output layer
+# prediction: map probability
+p1 <- predict(m1, newdata=preds)
+p1
 
-                   
+# plot the output
+plot(p1, col=cl)
+points(presences, pch=16)
+
+# final stack with everything
+s1 <- stack(preds, p1)
+plot(s1, col=cl)
+
+# let's decide names
+names(s1) <- c("Elevation", "Precipitation", "Temperature", "Vegetation", "Probability")
+plot(s1, col=cl)
+
                    
                    
                    
