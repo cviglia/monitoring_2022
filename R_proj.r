f37d75d53c3ce9bfccc5a1280baed9b6b0c16ac6
cviglia <92572568+cviglia@users.noreply.github.com>
2022-02-02 17:30:47 +0100

    Update R_proj.r

--- R_proj.r
+++ R_proj.r
@@ -1,28 +1,144 @@
 # my project
 
+# The Normalized Difference Vegetation Index (NDVI) is an indicator of the greenness of the biomes, a proxy to quantify the vegetation amount.
+
+# Even though it is not a physical property of the vegetation cover, its very simple formulation NDVI = (REF_nir – REF_red)/(REF_nir + REF_red) 
+# where REF_nir and REF_red are the spectral reflectances measured in the near infrared and red wavebands respectively, makes it widely used for ecosystems monitoring.
+
+# The NDVI is widely used by the bio-geophysical community to monitor the vegetation state and disturbances to address a large range of applications, 
+# including forestry, agriculture, food security, water management.
+
+# Very low values of NDVI (0.1 and below) correspond to barren areas of rock, sand, or snow. Moderate values represent shrub and grassland (0.2 to 0.3), 
+# while high values indicate temperate and tropical rainforests (0.6 to 0.8).
+
+
 library(ncdf4)
 library(raster)
 library(ggplot2)
 library(viridis)
 library(RStoolbox)
 library(patchwork)
+library(gridExtra)
 
 setwd("C:/lab//copernicus/") 
 
+######### 2021
+# raster
 veg20210101 <- raster("c_gls_NDVI300_202101010000_GLOBE_PROBAV_V1.0.1.nc")
 veg20210101
 plot(veg20210101)
 
+#crop
 ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-veg1_cropped <- crop(veg20210101, ext)
-plot(veg1_cropped <- crop(veg20210101, ext))
+veg8_cropped <- crop(veg20210101, ext)
+plot(veg8_cropped <- crop(veg20210101, ext))
+# plot(veg8_cropped, col=cl)
 
 
+######### 2020
+# raster
 veg20200101 <- raster("c_gls_NDVI300_202001010000_GLOBE_PROBAV_V1.0.1.nc")
 veg20200101
 plot(veg20200101)
 
+# crop
 ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-veg2_cropped <- crop(veg20200101, ext)
-plot(veg2_cropped <- crop(veg20200101, ext))
+veg7_cropped <- crop(veg20200101, ext)
+plot(veg7_cropped <- crop(veg20200101, ext))
+# plot(veg7_cropped, col=cl)
+
+
+######### 2019
+# raster
+veg20190101 <- raster("c_gls_NDVI300_201901010000_GLOBE_PROBAV_V1.0.1.nc")
+veg20190101
+plot(veg20190101)
+
+# crop
+ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
+veg6_cropped <- crop(veg20190101, ext)
+plot(veg6_cropped <- crop(veg20190101, ext))
+# plot(veg6_cropped, col=cl)
+
+
+######### 2018
+# raster
+veg20180101 <- raster("c_gls_NDVI300_201801010000_GLOBE_PROBAV_V1.0.1.nc")
+veg20180101
+plot(veg20180101)
+
+# crop
+ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
+veg5_cropped <- crop(veg20180101, ext)
+plot(veg5_cropped <- crop(veg20180101, ext))
+# plot(veg5_cropped, col=cl)
+
+
+######### 2017
+# raster
+veg20170101 <- raster("c_gls_NDVI300_201701010000_GLOBE_PROBAV_V1.0.1.nc")
+veg20170101
+plot(veg20170101)
+
+# crop
+ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
+veg4_cropped <- crop(veg20170101, ext)
+plot(veg4_cropped <- crop(veg20170101, ext))
+# plot(veg4_cropped, col=cl)
+
+
+######### 2016
+# raster
+veg20160101 <- raster("c_gls_NDVI300_201601010000_GLOBE_PROBAV_V1.0.1.nc")
+veg20160101
+plot(veg20160101)
+
+# crop
+ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
+veg3_cropped <- crop(veg20160101, ext)
+plot(veg3_cropped <- crop(veg20160101, ext))
+# plot(veg3_cropped, col=cl)
+
+
+######### 2015
+# raster
+veg20150101 <- raster("c_gls_NDVI300_201501010000_GLOBE_PROBAV_V1.0.1.nc")
+veg20150101
+plot(veg20150101)
+
+# crop
+ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
+veg2_cropped <- crop(veg20150101, ext)
+plot(veg2_cropped <- crop(veg20150101, ext))
+# plot(veg2_cropped, col=cl)
+
+
+######### 2014
+# raster
+veg20140101 <- raster("c_gls_NDVI300_201401010000_GLOBE_PROBAV_V1.0.1.nc")
+veg20140101
+plot(veg20140101)
+
+# crop
+ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
+veg1_cropped <- crop(veg20140101, ext)
+plot(veg1_cropped <- crop(veg20140101, ext))
+# plot(veg1_cropped, col=cl)
+
+
+
+veg <- stack(veg1_cropped, veg2_cropped, veg3_cropped, veg4_cropped, veg5_cropped, veg6_cropped, veg7_cropped, veg8_cropped)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
