aef24b4adafa6f92de2da28abedb029f99a9e8d4
cviglia <92572568+cviglia@users.noreply.github.com>
2022-02-03 13:28:29 +0100

    Update R_proj.r

--- R_proj.r
+++ R_proj.r
@@ -2,8 +2,8 @@
 
 # The Normalized Difference Vegetation Index (NDVI) is an indicator of the greenness of the biomes, a proxy to quantify the vegetation amount.
 
-# Even though it is not a physical property of the vegetation cover, its very simple formulation NDVI = (REF_nir – REF_red)/(REF_nir + REF_red) 
-# where REF_nir and REF_red are the spectral reflectances measured in the near infrared and red wavebands respectively, makes it widely used for ecosystems monitoring.
+# Even though it is not a physical property of the vegetation cover, it has a very simple formulation NDVI = (REF_nir – REF_red)/(REF_nir + REF_red) 
+# where REF_nir and REF_red are the spectral reflectances measured in the near infrared and red wavebands respectively, thqt makes it widely used for ecosystems monitoring.
 
 # The NDVI is widely used by the bio-geophysical community to monitor the vegetation state and disturbances to address a large range of applications, 
 # including forestry, agriculture, food security, water management.
@@ -17,6 +17,14 @@
 
 # The Global Climate Observing System (GCOS) specified the black-sky albedo as an Essential Climate Variable and the product is required for climate change purposes.
 
+# Albedo is a key forcing parameter controlling the planetary radiative energy budget and the partitioning of radiative energy between the atmosphere and surface. 
+
+# The energy absorbed at the surface is used to drive vegetation processes such as evapotranspiration, photosynthesis and carbon assimilation, 
+# and govern temperature-related processes such as evaporation, and snow melt. 
+
+# Thus albedo varies in space and time as a result of both natural processes (e.g. changes in solar position, inundation and vegetation growth) 
+# and human activities (e.g. clearing and planting forests, sowing and harvesting crops, burning rangeland). It is a sensitive indicator of environmental vulnerability. 
+
 
 library(ncdf4)
 library(raster)
@@ -28,30 +36,19 @@ library(gridExtra)
 
 setwd("C:/lab//copernicus/") 
 
-######### 2021 veg
-# raster
-veg20210101 <- raster("c_gls_NDVI300_202101010000_GLOBE_PROBAV_V1.0.1.nc")
-veg20210101
-plot(veg20210101)
-
-#crop
-ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-veg8_cropped <- crop(veg20210101, ext)
-plot(veg8_cropped <- crop(veg20210101, ext))
-# plot(veg8_cropped, col=cl)
-
-
 ######### 2020 veg
 # raster
 veg20200101 <- raster("c_gls_NDVI300_202001010000_GLOBE_PROBAV_V1.0.1.nc")
 veg20200101
 plot(veg20200101)
 
+veg20200701 <- raster
+
 # crop
 ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-veg7_cropped <- crop(veg20200101, ext)
-plot(veg7_cropped <- crop(veg20200101, ext))
-# plot(veg7_cropped, col=cl)
+veg6_cropped <- crop(veg20200101, ext)
+plot(veg6_cropped <- crop(veg20200101, ext))
+# plot(veg6_cropped, col=cl)
 
 
 ######### 2019 veg
@@ -62,9 +59,9 @@ plot(veg20190101)
 
 # crop
 ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-veg6_cropped <- crop(veg20190101, ext)
-plot(veg6_cropped <- crop(veg20190101, ext))
-# plot(veg6_cropped, col=cl)
+veg4_cropped <- crop(veg20190101, ext)
+plot(veg5_cropped <- crop(veg20190101, ext))
+# plot(veg5_cropped, col=cl)
 
 
 ######### 2018 veg
@@ -75,9 +72,9 @@ plot(veg20180101)
 
 # crop
 ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-veg5_cropped <- crop(veg20180101, ext)
-plot(veg5_cropped <- crop(veg20180101, ext))
-# plot(veg5_cropped, col=cl)
+veg4_cropped <- crop(veg20180101, ext)
+plot(veg4_cropped <- crop(veg20180101, ext))
+# plot(veg4_cropped, col=cl)
 
 
 ######### 2017 veg
@@ -88,9 +85,9 @@ plot(veg20170101)
 
 # crop
 ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-veg4_cropped <- crop(veg20170101, ext)
-plot(veg4_cropped <- crop(veg20170101, ext))
-# plot(veg4_cropped, col=cl)
+veg3_cropped <- crop(veg20170101, ext)
+plot(veg3_cropped <- crop(veg20170101, ext))
+# plot(veg3_cropped, col=cl)
 
 
 ######### 2016 veg
@@ -101,9 +98,9 @@ plot(veg20160101)
 
 # crop
 ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-veg3_cropped <- crop(veg20160101, ext)
-plot(veg3_cropped <- crop(veg20160101, ext))
-# plot(veg3_cropped, col=cl)
+veg2_cropped <- crop(veg20160101, ext)
+plot(veg2_cropped <- crop(veg20160101, ext))
+# plot(veg2_cropped, col=cl)
 
 
 ######### 2015 veg
@@ -114,69 +111,35 @@ plot(veg20150101)
 
 # crop
 ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-veg2_cropped <- crop(veg20150101, ext)
-plot(veg2_cropped <- crop(veg20150101, ext))
-# plot(veg2_cropped, col=cl)
-
-
-######### 2014 veg
-# raster
-veg20140101 <- raster("c_gls_NDVI300_201401010000_GLOBE_PROBAV_V1.0.1.nc")
-veg20140101
-plot(veg20140101)
-
-# crop
-ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-veg1_cropped <- crop(veg20140101, ext)
-plot(veg1_cropped <- crop(veg20140101, ext))
+veg1_cropped <- crop(veg20150101, ext)
+plot(veg1_cropped <- crop(veg20150101, ext))
 # plot(veg1_cropped, col=cl)
 
 
 
-veg <- stack(veg1_cropped, veg2_cropped, veg3_cropped, veg4_cropped, veg5_cropped, veg6_cropped, veg7_cropped, veg8_cropped)
+veg <- stack(veg1_cropped, veg2_cropped, veg3_cropped, veg4_cropped, veg5_cropped, veg6_cropped)
 plot(veg)
 
 
 pdf()
 par(mfrow=c(2,2))
-plot(veg1_cropped, main="2014")
-plot(veg2_cropped, main="2015")
-plot(veg3_cropped, main="2016")
-plot(veg4_cropped, main="2017")
+plot(veg1_cropped, main="2015")
+plot(veg2_cropped, main="2016")
+plot(veg3_cropped, main="2017")
 
 par(mfrow=c(2,2))
-plot(veg5_cropped, main="2018")
-plot(veg6_cropped, main="2019")
-plot(veg7_cropped, main="2020")
-plot(veg8_cropped, main="2021")
+plot(veg4_cropped, main="2018")
+plot(veg5_cropped, main="2019")
+plot(veg6_cropped, main="2020")
 dev.off()
 
-par(mfrow=c(2,4))
-plot(veg1_cropped, main="2014")
-plot(veg2_cropped, main="2015")
-plot(veg3_cropped, main="2016")
-plot(veg4_cropped, main="2017")
-plot(veg5_cropped, main="2018")
-plot(veg6_cropped, main="2019")
-plot(veg7_cropped, main="2020")
-plot(veg8_cropped, main="2021")
-
-legend(x, y,   # Coordinates (x also accepts keywords)
-       legend, # Vector with the name of each group
-       fill,   # Creates boxes in the legend with the specified colors
-       col = par("col"), # Color of lines or symbols
-       border = "black", # Fill box border color
-       lty, lwd,         # Line type and width
-       pch,              # Add pch symbols to legend lines or boxes
-       bty = "o",        # Box type (bty = "n" removes the box)
-       bg = par("bg")    # Background color of the legend
-       box.lwd = par("lwd"), # Legend box line width
-       box.lty = par("lty"), # Legend box line type
-       box.col = par("fg"),  # Legend box line color
-       cex = 1,          # Legend size
-       horiz = FALSE     # Horizontal (TRUE) or vertical (FALSE) legend
-       title = NULL      # Legend title
-)
+par(mfrow=c(2,3))
+plot(veg1_cropped, main="2015")
+plot(veg2_cropped, main="2016")
+plot(veg3_cropped, main="2017")
+plot(veg4_cropped, main="2018")
+plot(veg5_cropped, main="2019")
+plot(veg6_cropped, main="2020")
 
 
 ggplot(veg, aes(x=longitude, y=latitude, color=cover)) + geom_bar(stat="identity", fill="white")
@@ -191,8 +154,8 @@ plot(alb20200124)
 
 # crop
 ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-alb7_cropped <- crop(alb20200124, ext)
-plot(alb7_cropped <- crop(alb20200124, ext))
+alb6_cropped <- crop(alb20200124, ext)
+plot(alb6_cropped <- crop(alb20200124, ext))
 
 
 ######### 2019 alb
@@ -203,8 +166,8 @@ plot(alb20190124)
 
 # crop
 ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-alb6_cropped <- crop(alb20190124, ext)
-plot(alb6_cropped <- crop(alb20190124, ext))
+alb5_cropped <- crop(alb20190124, ext)
+plot(alb5_cropped <- crop(alb20190124, ext))
 
 
 ######### 2018 alb
@@ -215,8 +178,8 @@ plot(alb20180124)
 
 # crop
 ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-alb5_cropped <- crop(alb20180124, ext)
-plot(alb5_cropped <- crop(alb20180124, ext))
+alb4_cropped <- crop(alb20180124, ext)
+plot(alb4_cropped <- crop(alb20180124, ext))
 
 
 ######### 2017 alb
@@ -227,66 +190,56 @@ plot(alb20170124)
 
 # crop
 ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-alb4_cropped <- crop(alb20170124, ext)
-plot(alb4_cropped <- crop(alb20170124, ext))
+alb3_cropped <- crop(alb20170124, ext)
+plot(alb3_cropped <- crop(alb20170124, ext))
 
 
 ######### 2016 alb
 # raster
-alb20160124 <- raster("")
+alb20160124 <- raster("c_gls_ALDH_201601240000_GLOBE_PROBAV_V1.5.1.nc")
 alb20160124
 plot(alb20160124)
 # crop
 ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-alb3_cropped <- crop(alb20160124, ext)
-plot(alb3_cropped <- crop(alb20160124, ext))
+alb2_cropped <- crop(alb20160124, ext)
+plot(alb2_cropped <- crop(alb20160124, ext))
 
 
 ######### 2015 alb
 # raster
-alb20150124 <- raster("")
+alb20150124 <- raster("c_gls_ALDH_201501240000_GLOBE_PROBAV_V1.5.1.nc")
 alb20150124
 plot(alb20150124)
 # crop
 ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-alb2_cropped <- crop(alb20150124, ext)
-plot(alb2_cropped <- crop(alb20150124, ext))
+alb1_cropped <- crop(alb20150124, ext)
+plot(alb1_cropped <- crop(alb20150124, ext))
 
 
-######### 2014 alb
-# raster
-alb20140124 <- raster("")
-alb20140124
-plot(alb20140124)
-# crop
-ext <- c(11.9256, 15.6528, 35.4929, 38.8122)
-alb1_cropped <- crop(alb20140124, ext)
-plot(alb1_cropped <- crop(alb20140124, ext))
 
-
-alb <- stack(alb1_cropped, alb2_cropped, alb3_cropped, alb4_cropped, alb5_cropped, alb6_cropped, alb7_cropped)
+alb <- stack(alb1_cropped, alb2_cropped, alb3_cropped, alb4_cropped, alb5_cropped, alb6_cropped)
 plot(alb)
 
+
+
 par(mfrow=c(2,4))
-plot(alb1_cropped, main="2014")
-plot(alb2_cropped, main="2015")
-plot(alb3_cropped, main="2016")
-plot(alb4_cropped, main="2017")
-plot(alb5_cropped, main="2018")
-plot(alb6_cropped, main="2019")
-plot(alb7_cropped, main="2020")
+plot(alb1_cropped, main="2015")
+plot(alb2_cropped, main="2016")
+plot(alb3_cropped, main="2017")
+plot(alb4_cropped, main="2018")
+plot(alb5_cropped, main="2019")
+plot(alb6_cropped, main="2020")
 
 
 pdf()
 par(mfrow=c(2,2))
-plot(alb1_cropped, main="2014")
-plot(alb2_cropped, main="2015")
-plot(alb3_cropped, main="2016")
-plot(alb4_cropped, main="2017")
+plot(alb1_cropped, main="2015")
+plot(alb2_cropped, main="2016")
+plot(alb3_cropped, main="2017")
 
-plot(alb5_cropped, main="2018")
-plot(alb6_cropped, main="2019")
-plot(alb7_cropped, main="2020")
+plot(alb4_cropped, main="2018")
+plot(alb5_cropped, main="2019")
+plot(alb6_cropped, main="2020")
 dev.off()
 
 
