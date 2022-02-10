1ca8fe893097eb5ebacdbd1ae8c74dc75f2c2859
cviglia <92572568+cviglia@users.noreply.github.com>
2022-02-10 13:27:09 +0100

    Update R_proj.r

--- R_proj.r
+++ R_proj.r
@@ -39,6 +39,26 @@
 # riserva cavagrande long lat (15.0969771982218,36.9768781858837)
 
 
+library(ggplot2)
+library(raster)
+data(lsat)
+
+## Calculate NDVI
+ndvi <- spectralIndices(lsat, red = "B3_dn", nir = "B4_dn", indices = "NDVI")
+ndvi
+ggR(ndvi, geom_raster = TRUE) +
+        scale_fill_gradientn(colours = c("black", "white")) 
+
+## Calculate all possible indices, given the provided bands 
+## Convert DNs to reflectance (required to calculate EVI and EVI2)
+mtlFile  <- system.file("external/landsat/LT52240631988227CUB02_MTL.txt", package="RStoolbox")
+lsat_ref <- radCor(lsat, mtlFile, method = "apref")
+
+SI <- spectralIndices(lsat_ref, red = "B3_tre", nir = "B4_tre")
+plot(SI)
+
+
+
 # libraries
 pacman::p_load(sp, rgdal, raster, rgeos, rasterVis, 
                RStoolbox, dplyr, writexl, ggplot2, ncdf4)
